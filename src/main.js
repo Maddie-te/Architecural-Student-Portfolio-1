db20134eb4e1154f233d34b301d00e5fa6d691f8
Maddie-te <maddie.teeple@ucalgary.ca>
2025-01-20 02:57:46 -0700

    Updated portfolio

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import "./style.css";
 import data from "./data/data";
 import { createThreeScene } from "./threeScene";
 
-const cubes = [
-  "./cubeDrawings/cube1.webp",
-  "./cubeDrawings/cube2.webp",
-  "./cubeDrawings/cube3.webp",
-  "./cubeDrawings/cube4.webp",
+const tags = [
+  "./tagPics/tag2.png",
+  "./tagPics/tag3.png",
+  "./tagPics/tag4.png",
+  "./tagPics/REF.png",
 ];
 
 const trees = [
@@ -14,6 +14,13 @@ const trees = [
   "./treeDrawings/tree2.jpg",
   "./treeDrawings/tree3.jpg",
   "./treeDrawings/tree4.jpg",
+];
+
+const cubes = [
+  "./cubeDrawings/cube1.webp",
+  "./cubeDrawings/cube2.webp",
+  "./cubeDrawings/cube3.webp",
+  "./cubeDrawings/cube4.webp",
 ];
 
 document.querySelector("#app").innerHTML = `
@@ -25,22 +32,23 @@ document.querySelector("#app").innerHTML = `
 
     <section id="projects">
       <div id="project-row">
-      Model 1
+      Cadavre Exquis
         <div class="three-model">
-          <div id="model1"></div>
+          <img src="./tagPics/tag1.png" id="model1" alt="Model 1 Image" />
         </div>
         <div id="images-description">
           <div id="images">
-            ${cubes
+            ${tags
               .map(
-                (cube, index) => `<img src="${cube}" alt="cube${index + 1}" />`
+                (tag, index) => `<img src="${tag}" alt="cube${index + 1}" />`
               )
               .join("")}
           </div>
-          <h4 id="description">A cubic 3D model is a geometric representation of a cube, consisting of six equal square faces joined at right angles. Each edge of the cube is of equal length, forming a symmetrical, rigid structure. The model can be textured, colored, or transparent, and it may include variations such as beveled edges or hollow interiors, depending on its intended purpose in design, animation, or engineering.</h4>
+          <h4 id="description">Revelation: A World of Unlimited Discovery, is a celebration of human curiosity and a warning as to what isolation can cause. Our lives are a mosaic of discovery, learning not only from the endless wonders of the earth, but also from the unique perspectives of others.</h4>
         </div>
       </div>
 
+      ${/* Model 2 commented out 
       <div id="project-row">
       Model 2
         <div class="three-model">
@@ -59,6 +67,8 @@ document.querySelector("#app").innerHTML = `
         </div>
       </div>
 
+*/ ""}
+${/* Model 2 commented out 
       <div id="project-row">
       Model 3
         <div class="three-model">
@@ -75,6 +85,8 @@ document.querySelector("#app").innerHTML = `
           <h4 id="description">A 3D house model is a geometric representation of a house, featuring walls, a roof, and windows. It often includes additional details such as doors, chimneys, and balconies, with textures like brick, wood, or stucco to enhance realism. The interior may also feature rooms, furniture, and lighting for a more detailed design.</h4>
         </div>
       </div>
+      */ ""}
+
     </section>
 
     <ul id="footer-items">
@@ -91,6 +103,6 @@ document.querySelector("#app").innerHTML = `
 `;
 
 // Create three.js scenes for each
-createThreeScene("#model1", "/3DModels/project1/cube.obj");
-createThreeScene("#model2", "/3DModels/project2/tree.obj");
-createThreeScene("#model3", "/3DModels/project3/cottage.obj");
+//createThreeScene("#model1", "/3DModels/project1/cube.obj");
+//createThreeScene("#model2", "/3DModels/project2/tree.obj");
+//createThreeScene("#model3", "/3DModels/project3/cottage.obj");
